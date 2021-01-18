c27bb99bd448fa839cc9dc9d131fd1feb78fb20f
Rich Mealey <rich.mealey@gmail.com>
2021-01-18 12:35:50 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send("I never really did mind about the little things!'));
+app.get('/', (req, res) => res.send("This is no little thing - it's my future!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
